6f9e05a671947f27063b2ce096feae39dbc16152
willianhenri <wh341873@gmail.com>
2025-08-28 13:10:08 -0300

    AddFileMigration

---
+++ MeuPetshop.Infrastructure/Migrations/20250828154517_AdicionaColunaDateAdded.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MeuPetshop.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AdicionaColunaDateAdded : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Quantidade",
+                table: "Produtos",
+                newName: "StockQuantity");
+
+            migrationBuilder.RenameColumn(
+                name: "Preco",
+                table: "Produtos",
+                newName: "Price");
+
+            migrationBuilder.RenameColumn(
+                name: "Nome",
+                table: "Produtos",
+                newName: "Name");
+
+            migrationBuilder.RenameColumn(
+                name: "Descricao",
+                table: "Produtos",
+                newName: "Description");
+
+            migrationBuilder.RenameColumn(
+                name: "DataCadastro",
+                table: "Produtos",
+                newName: "DateAdded");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "StockQuantity",
+                table: "Produtos",
+                newName: "Quantidade");
+
+            migrationBuilder.RenameColumn(
+                name: "Price",
+                table: "Produtos",
+                newName: "Preco");
+
+            migrationBuilder.RenameColumn(
+                name: "Name",
+                table: "Produtos",
+                newName: "Nome");
+
+            migrationBuilder.RenameColumn(
+                name: "Description",
+                table: "Produtos",
+                newName: "Descricao");
+
+            migrationBuilder.RenameColumn(
+                name: "DateAdded",
+                table: "Produtos",
+                newName: "DataCadastro");
+        }
+    }
+}
